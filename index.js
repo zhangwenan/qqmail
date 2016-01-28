0dd390ce242b0c2eab83b6b4dea956437244e3be
wenlie <88306691@qq.com>
2016-01-28 21:03:13 +0800

    免验证码的情况，登陆成功

--- index.js
+++ index.js
@@ -19,6 +19,14 @@ var qqmail = {
   g_pt_ver_md5: null,
   g_login_sig: null,
 
+  g_need_vcode: true,
+  g_salt:null,
+  g_pt_verifysession_v1: null,
+  g_vcode: {
+    result: null
+  },
+
+
   temp:{
 
   },
@@ -70,11 +78,8 @@ var qqmail = {
       iframe_reg.exec(data);
       if(RegExp.$1){
         iframe_url = RegExp.$1;
-        console.log(iframe_url);
         self.temp.iframe = iframe_url;
       }
-
-      console.log(headers);
 
       self.visitIframe();
 
@@ -89,20 +94,12 @@ var qqmail = {
    */
   visitIframe: function(){
     var self = this;
-    var headers_sent = {
-      /*"Host": "ui.ptlogin2.qq.com",
-      "Accept-Encoding": "gzip, deflate",
-      "Accept-Language": "zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
-      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*!/!*;q=0.8",
-      "Content-Type": "",
-      "X-Powered-By": "",
-      "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:41.0) Gecko/20100101 Firefox/41.0"*/
-    }
+    var headers_sent = {};
 
-    console.log('开始访问' + self.temp.iframe);
     nodegrass.get(self.temp.iframe, function(data, status, headers){
 
       self.cookies = headers["set-cookie"];
+      console.log(self.cookies);
 
       var login_sig_reg = /pt_login_sig=([^;]+)/;
 
@@ -124,13 +121,7 @@ var qqmail = {
         self.g_daid = ptui.daid;
         self.g_pt_ver_md5 = ptui.pt_ver_md5;
 
-        /*console.log(self.g_version);
-        console.log(self.g_jsversion);
-        console.log(self.g_appid);
-        console.log(self.g_lang);
-        console.log(self.g_style);
-        console.log(self.g_daid);
-        console.log(self.g_pt_ver_md5);*/
+
         self.checkVCode();
       }
       else{
@@ -141,6 +132,35 @@ var qqmail = {
     }, headers_sent, "utf8").on('error', function(e) {
       console.log("Got error: " + e.message);
     });
+  },
+
+  postToLogin: function(ptvfsession, p){
+
+    var self = this;
+    var url = 'https://ssl.ptlogin2.qq.com/login?u=' + self.qq + '&verifycode=' + self.g_vcode.result +'&pt_vcode_v1=0' +
+      '&pt_verifysession_v1=' + ptvfsession + '&p=' + p + '&pt_randsalt=0&' +
+      'u1=https%3A%2F%2Fmail.qq.com%2Fcgi-bin%2Flogin%3Fvt%3Dpassport%26vm%3Dwpt%26ft%3Dloginpage%26target%3D%26account%3D'+
+      self.qq + '&ptredirect=1&h=1&t=1&g=1&from_ui=1&ptlang=' + self.g_lang + '&action=' + self.g_action +
+      '&js_ver=' + self.g_jsversion + '&js_type=1' +
+      '&login_sig=' + self.g_login_sig + '&pt_uistyle=' + self.g_style + '&aid=' + self.g_appid + '&daid=' + self.g_daid + '&';
+
+
+    var header_sent = {
+      "Host": "ssl.ptlogin2.qq.com",
+      "Referer": "https://xui.ptlogin2.qq.com/cgi-bin/xlogin?appid=" + self.g_appid + "&daid=" + self.g_daid
+      + "&s_url=https://mail.qq.com/cgi-bin/login?vt=passport%26vm=wpt%26ft=loginpage%26target=&style=" + self.g_style
+      + "&low_login=1&proxy_url=https://mail.qq.com/proxy.html&need_qr=0&;hide_border=1&border_radius=0"
+      + "&self_regurl=http://zc.qq.com/chs/index.html?type=1&app_id=11005?t=regist"
+      + "&pt_feedback_link=http://support.qq.com/discuss/350_1.shtml"
+      + "&css=https://res.mail.qq.com/zh_CN/htmledition/style/ptlogin_input24e6b9.css",
+      "cookie": self.cookies
+    };
+
+    nodegrass.get(url, function(data, status, headers){
+      console.log(data);
+      self.cookies = self.cookies.concat(headers["set-cookie"]);
+
+    }, header_sent, "utf8");
   },
 
   checkVCode: function(){
@@ -158,14 +178,52 @@ var qqmail = {
       + "&pt_feedback_link=http://support.qq.com/discuss/350_1.shtml"
       + "&css=https://res.mail.qq.com/zh_CN/htmledition/style/ptlogin_input24e6b9.css",
       "cookie": self.cookies
-    }
+    };
 
     nodegrass.get(url, function(data, status, headers){
-      console.log(data);
-      console.log(status);
-      console.log(headers);
+      eval('self.' + data);
+      self.cookies = self.cookies.concat(headers["set-cookie"]);
 
+      // 无需图形验证码
+      if(!self.g_need_vcode){
+        var ptvfsession = self.get_ptvfsession();
+        var p = encoder.encode(self.pwd, self.g_salt, self.g_vcode.result);
+        self.postToLogin(ptvfsession, p);
+      }
+      else{
+
+      }
     }, header_sent, "utf8");
+  },
+
+
+
+  ptui_checkVC: function(a, b, c, d, e){
+    var self = this;
+    self.g_need_vcode = !(a == '0');  // 0代表无需验证码
+    self.g_vcode = { result: b };
+    self.g_salt = c;
+    self.g_pt_verifysession_v1 = d;
+  },
+
+
+  get_ptvfsession: function(){
+    var self = this;
+    if(self.g_pt_verifysession_v1){
+      return self.g_pt_verifysession_v1;
+    }
+    else{
+      var reg = /ptvfsession=([^;]+);/ig;
+      var reg2 = /verifysession=([^;]+);/ig;
+
+      if(reg.exec(self.cookies)){
+        return RegExp.$1;
+      }
+      else if(reg2.exec(self.cookies)){
+        return RegExp.$1;
+      }
+    }
+    return '';
   }
 };
 
